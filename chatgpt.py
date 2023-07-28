ee91c4d28d5f262023e1afd0a6a7ae5d57424f1c
Sergio GAMA <sergiogama@hotmail.com>
2023-07-28 17:27:11 -0300

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -6,28 +6,29 @@ import pdfplumber
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 
+input_file = 'data.txt'  # Define o arquivo de entrada
 # Verifica se os dados de entrada estão no formato PDF ou formato de texto simples
 pdf = 0  # Defina pdf para 1 se os dados estiverem no formato PDF, caso contrário, defina como 0 (assumindo que é texto simples)
 if pdf == 1:
     # Se os dados estiverem no formato PDF, extrai o texto do PDF e salva em um arquivo local
     pdf_file = 'Reforma-Tributaria-2023.pdf'
+    input_file = 'data_from_pdf.txt'
     with pdfplumber.open(pdf_file) as pdf:
         # Remove o arquivo local se já existir
-        if os.path.exists('data_from_pdf.txt'):
-            os.remove('data_from_pdf.txt')
+        if os.path.exists(input_file):
+            os.remove(input_file)
 
         # Lê todas as páginas do PDF e salva em um arquivo local
         for i in range(0, len(pdf.pages)):
             page = pdf.pages[i]
             text = page.extract_text()
 
-            with open('data_from_pdf.txt', 'a') as f:
+            with open(input_file, 'a') as f:
                 f.write(text)
                 f.write('\n')
                 f.close()
-    loader = TextLoader('data_from_pdf.txt')  # Cria um carregador de texto para os dados do PDF
-else:
-    loader = TextLoader('data.txt', autodetect_encoding=True)  # Cria um carregador de texto para os dados de texto simples
+                
+loader = TextLoader(input_file, autodetect_encoding=True)  # Cria um carregador de texto para os dados de texto simples
 
 # Cria um índice usando o VectorstoreIndexCreator
 index = VectorstoreIndexCreator().from_loaders([loader])
